e12c2a985845e2a13fd804310f78fc678f70e014
screening25 <screening@kakao.com>
2025-11-18 15:26:20 +0900

    Title: 세 실수의 반올림, Time: 142ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251118/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        double num3 = sc.nextDouble();
+        
+        System.out.printf("%.3f\n", num1);
+        System.out.printf("%.3f\n", num2);
+        System.out.printf("%.3f\n", num3);
+    }
+}
