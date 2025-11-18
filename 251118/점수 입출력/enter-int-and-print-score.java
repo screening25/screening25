3d9298c9cf7caf6326bbfaec49d558803b25af13
screening25 <screening@kakao.com>
2025-11-18 14:11:33 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251118/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextint();
+        System.out.println("Your score is " + age + "point.");
+    }
+}
