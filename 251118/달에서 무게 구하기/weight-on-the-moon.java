4fd2906c23bdc4c1203a0dcdf6b0e3e35f796fda
screening25 <screening@kakao.com>
2026-01-07 16:36:18 +0900

    Delete 251118 directory

--- 251118/달에서 무게 구하기/weight-on-the-moon.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int a = 13;
-        double b = 0.165;
-
-        System.out.printf("%d * %.6f = %.6f\n", a, b, a * b);
-    }
-}
--- 251118/점수 입출력/enter-int-and-print-score.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        System.out.println("Your score is " + age + " point.");
-    }
-}
