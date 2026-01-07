a90763166fd120767b472c2c376ce26019d1b529
screening25 <screening@kakao.com>
2026-01-07 16:37:34 +0900

    Delete java directory

--- java/Algorithm/src/boj10171.java
+++
@@ -1,8 +0,0 @@
-public class boj10171 {
-	public static void main(String[] args) {
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
-	}
-}
--- java/Practice/src/p20251201/forPractice04.java
+++
@@ -1,9 +0,0 @@
-package p20251201;
-public class forPractice04 {
-    public static void main(String[] args) {
-        // Please write your code here.
-        for (int i=5 ; i<=17; i++){
-            System.out.print(i+" ");
-        }
-    }
-}
