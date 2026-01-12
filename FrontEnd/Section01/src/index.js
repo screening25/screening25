1bb4c3e62702d09e85a23aa825b2f1479c91474c
윤상영 <buddyyun1@naver.com>
2026-01-12 23:46:34 +0900

    2026-01-12

--- FrontEnd/Section01/src/index.js
+++ FrontEnd/Section01/src/index.js
@@ -1,8 +1,11 @@
-// CommonJS Module
-// const moduleData = require('./math');
-// const {add, sub} = require('./math');   
+// // CommonJS Module
+// // const moduleData = require('./math');
+// // const {add, sub} = require('./math');   
 
-// ES6 Module
-import {add, sub} from './math.js';
-console.log(add(5, 3)); // Outputs: 8    
-console.log(sub(5, 3)); // Outputs: 2    
+// // ES6 Module
+// import {add, sub} from './math.js';
+// console.log(add(5, 3)); // Outputs: 8    
+// console.log(sub(5, 3)); // Outputs: 2    
+
+import randomColor from 'randomcolor'; // 라이브러리에서 가져올 때는 라이브러리 이름만 적으면 됨 
+console.log(randomColor()); // 무작위 색상 출력
