c346a627524ec537e65cd3774c1469f3e25cfdbd
윤상영 <buddyyun1@naver.com>
2026-01-19 23:56:56 +0900

    2025-01-19

--- FrontEnd/Section03/src/components/Button.jsx
+++ FrontEnd/Section03/src/components/Button.jsx
@@ -1,7 +1,19 @@
 // 기본값(default parameter) 설정 및 구조 분해 할당
 const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e)
+  //   console.log(text)
+  }
+
   return (
-    <button style={{ color: color }}>
+    // <button = {onClickButton}> // 다른 이벤트도 이렇게 사용 가능  
+    <button
+      onClick={()=>{  // Event가 발생했을 때 실질적으로 처리하는 함수 : EventHandler
+      
+        console.log(text)
+      }}
+
+    style={{ color: color }}>
       {text}
       {children}
     </button>
